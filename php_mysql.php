eddab9145441aa128b1a38786351911aa4f0ce6d
Lucas Carrington <carringl@onid.oregonstate.edu>
2015-02-15 14:33:07 -0800

    Might as well be the final, stuck and dont understand it enough to compile an answer

--- php_mysql.php
+++ php_mysql.php
@@ -1,4 +1,5 @@
 <?php
+
 class connectionTest {
 public $servername = "oniddb.cws.oregonstate.edu";
 public $username = "carringl-db";
@@ -6,33 +7,75 @@ public $password = "10VGEvyfesvu52XN";
 public $database = "carringl-db";}
 
 $connection = new connectionTest;
-//create connection
 $db = new mysqli($connection->servername, $connection->username, $connection->password, $connection->database);
 
 //check connection
 if ($db->connect_error) {
     die("connection failed: " . $db->connect_error);}
-//else{
-    //echo "Connection Successful";}
 
-//mysqli_selsect_db("carringl-db");
-$sql = $db->query("SELECT name, category, length, rented FROM videoCheckout");
+$sql = $db->query("SELECT id, name, category, length, irented FROM videoCheckout");
 echo "<table>";
 echo "<thead>";
 echo "<tr>";
+echo "<td> ID </td>";
 echo "<td> name </td>";
 echo "<td> category </td>";
 echo "<td> length </td>";
-echo "<td> rented </td>";
+//echo "<td> rented </td>";
+echo "<td> <form method='POST'><input type='submit' name= 'delAll' value = 'Delete All Records'></form>";  
+if(isset($_POST['delAll'])){
+    //echo "this works";
+    $query = "DELETE FROM videoCheckout";
+    $stmt = $db->prepare($query);
+    $stmt->bind_param('s', $row->name);
+    if($stmt->execute()){
+        echo "All Records Successfully Deleted";}
+    $stmt->close();}
 echo "</tr> </thead>";
 while($row = $sql->fetch_object()){
     echo "<tr>";
+    echo "<td> $row->id </td>";
     echo "<td> $row->name</td>";
     echo "<td> $row->category</td>";
     echo "<td> $row->length</td>";
-    echo "<td> $row->rented</td>";
-   // echo "<td> <form method='POST'> <input type= 'submit' value ='Delete Record'> </form></td>";
-    echo "</tr>";}
+    //$rented = 0;
+    echo "<td>";
+    if($row->irented == 0){
+        echo "<form method='POST'><input type='submit' name = 'rent_check' value='Check Out'></form>";
+        if (isset($_POST['rent_check'])) {
+            $trented = 1;
+            $ROWID = $row->id;
+            $query1 = "UPDATE videoCheckout SET irented=? WHERE id=?";
+            $stmt = $db->prepare($query1);
+            $stmt->bind_param('ii', $trented, $ROWID);
+            if($stmt->execute()){
+                echo "Record Saved.";}
+            $stmt->close();
+    }}
+    elseif($row->irented == 1){
+        echo "<form method='POST'><input type='submit' name='rent_check2' value='Check In'></form>";
+        if (isset($_POST['rent_check2'])) {
+            $trented = 0;
+            $ROWID = $row->id;
+            $query1 = "UPDATE videoCheckout SET irented=? WHERE id=?";
+            $stmt = $db->prepare($query1);
+            $stmt->bind_param('ii', $trented, $ROWID);
+            if($stmt->execute()){
+                echo "Record Saved.";}
+            $stmt->close();
+    }}
+    echo '</td>';
+    echo "<td> <form method='POST'><input type='submit' name= 'check1' value = 'Delete Record'></form>";  
+    if(isset($_POST['check1'])){
+             //echo "this works";
+             $query = "DELETE FROM videoCheckout VALUES (?)";
+             $stmt = $db->prepare($query);
+             $stmt->bind_param('s', $row->name);
+             if($stmt->execute()){
+                 echo "Success!";}
+             $stmt->close();}
+        
+   echo "</tr>";}
 echo "</table>";
 
 
@@ -41,25 +84,34 @@ echo "CHECK-IN MOVIE HERE<br>";
 echo "name<input type='text' maxlength='255' name='name' ></br>";
 echo "category<input type='text' maxlength='255' name='category'></br>";
 echo "length<input type='number' name='length'></br>";
-echo "rented<input type='checkbox' name='rented'></br>";
-echo "<input type='submit' name='submit' value='Deposit Movie'>";   
+//echo "rented<input type='checkbox' name='rented'></br>";
+echo "<input type='submit' name='new_submit' value='Deposit Movie'>";   
 echo "</form>";
 
+function rented($irented) {
+if($rented == 0){
+echo "<input type='submit' name = 'rent_check' value='Check Out'>";
+    if (isset($_POST['rent_check'])) {
+    $rented = 1;}}
+elseif($rented == 1){
+echo "<input type='submit' name='rent_check2' value='Check In'>";
+    if (isset($_POST['rent_check2'])) {
+    $rented = 0;}}}
+
+function insertData($db) {
 $tname = $_POST['name'];
 $tcategory = $_POST['category'];
 $tlength = $_POST['length'];
-$trented = $_POST['rented'];
-//insert();
-
-//function insert(){
-$query = "INSERT INTO videoCheckout (name, category, length, rented) VALUES (?,?,?,?)";
+//$trented = $_POST['rented'];
+$query = "INSERT INTO videoCheckout (name, category, length) VALUES (?,?,?)";
 $stmt = $db->prepare($query);
-$stmt->bind_param('ssib', $tname, $tcategory, $tlength, $trented);
-
+$stmt->bind_param('ssi', $tname, $tcategory, $tlength);
 if($stmt->execute()){
-    echo "Success!";}
-//$newId = $ssmt->insert_id;
+    echo "Record Saved.";}
 $stmt->close();
+}
+if(isset($_POST['new_submit'])){
+    insertData($db);}
 
 $dbi->close();
 ?>
